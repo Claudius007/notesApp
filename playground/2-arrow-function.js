af101b51da1dcfc6513e3f4e4ec3a59003163a2f
Claudius Daniel <mofehintoluwaclaudius@gmail.com>
2019-12-07 19:34:16 -0500

    dumped old codes

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,29 @@
+// if (command === 'add'){
+//     log('Adding note!')
+// }
+// else if(command === 'remove'){
+//     log('Removing note!')
+// }
+//const command = process.argv
+//log (command)
+
+// log ('Title: '+argv.title);
+// log ('Body: '+argv.body);
+
+// const log = console.log
+// const validator = require('validator')
+
+// const msg = getNotes()
+
+// log(msg);
+
+// log(validator.isURL('https://carehop'))
+
+// log (chalk.bold.inverse.red('Success!'))
+
+// log(chalk.inverse.underline('Wawuuu'))
+// const add = require('./utils.js')
+
+// const sum = add(4,4)
+
+// console.log(sum);
